7a5a780e15abb916a1f596cb5afcf4992bf08b12
davidalejo04 <138809407+davidalejo04@users.noreply.github.com>
2026-02-01 23:28:04 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -480,7 +480,7 @@ if uploaded_file:
         conc=pd.merge(df_full.groupby('Categoria')['Ganancias'].sum().sort_values(ascending=False),df_full[df_full['Ganancias']<0].groupby('Categoria')['Ganancias'].sum().sort_values(ascending=False),on='Categoria',how='left')
         (conc.rename(columns={'Ganancias_x':'Ganancias_totales','Ganancias_y':'Perdidas SKU con ganancias negativas'}))
     
-        st.write("La Utilidad Neta DESCARTANDO SKU Fantasma y Transaccion_ID fantasma son de...",f"${(df_full[df_full['Bodega_Origen'].notnull]['Ganancias'].sum()):,.2f}")
+        st.write("La Utilidad Neta DESCARTANDO SKU Fantasma y Transaccion_ID fantasma son de...",f"${(df_full[df_full['Bodega_Origen'].notnull()]['Ganancias'].sum()):,.2f}")
         st.write("La Utilidad Neta DESCARTANDO solo SKU Fantasma...",f"${(df_rich[df_rich['SKU_ID'].notnull()]['Ganancias2'].sum()):,.2f}")
         st.write("La Utilidad Neta TOMANDO SKU Fantasma y Transaccion_ID fantasma son de...",f"${(df_full['Ganancias'].sum()):,.2f}")
         st.write("impacto casi del 75%!!... los datos elimiandos son considerables y esto debe ser tomado en cuenta en el analisis")
